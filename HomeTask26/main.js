adfc6be65464fba7bf5bc7901934896ef9f34ab1
Igor <62031924+SpaceHolderdJs@users.noreply.github.com>
2021-01-26 18:57:52 +0200

    Add files via upload

---
+++ HomeTask26/main.js
@@ -0,0 +1,32 @@
+//Task-1
+const JSONCreator = (obj => JSON.stringify({ keys : Object.keys(obj), values : Object.values(obj)}));
+    
+console.log(JSONCreator({
+    name: "Igor",
+    age : 21
+}));
+
+function flatMap(obj){
+
+   for(let i in obj){
+     typeof obj[i] == "object" && [Object.keys(obj[i]).forEach((e,indx)=>obj[e] = Object.values(obj[i])[indx]), delete obj[i]]
+  }
+   return console.log(JSON.stringify(obj));
+}
+
+//Task-2
+flatMap({
+  name : "Igor",
+  age : 21,
+  skills : {
+             web:"html",
+             logic:"Js",
+             interface:"React"
+           }
+});
+
+const obj = {
+  name : "Yulenka",
+  age : 100,
+  isPretty : true
+}
